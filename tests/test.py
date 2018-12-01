4db88e9cadd4de10a36a8e0058a0007f92ed96e8
AltTether <200351p@ugs.kochi-tech.ac.jp>
2018-12-02 03:14:16 +0900

    [add] add test for accounts(followers,following,statuses,following,follow,unfollow,block,unblock)

--- tests/test.py
+++ tests/test.py
@@ -26,3 +26,31 @@ class TestApi(unittest.TestCase):
         response = msdn.accounts.update_credentials(display_name=display_name)
         updated_display_name = json.loads(response.text)['display_name']
         self.assertEqual(updated_display_name, display_name)
+
+    def test_getting_account_followers(self):
+        response = msdn.accounts._id.followers(_id='')
+        self.assertEqual(response.status_code, 200)
+
+    def test_getting_who_account_following(self):
+        response = msdn.accounts._id.following(_id='')
+        self.assertEqual(response.status_code, 200)
+
+    def test_getting_account_statuses(self):
+        response = msdn.accounts._id.statuses(_id='')
+        self.assertEqual(response.status_code, 200)
+
+    def test_following_account(self):
+        response = msdn.accounts._id.follow(_id='')
+        self.assertEqual(response.status_code, 200)
+
+    def test_following_unfollowing_account_reblogs(self):
+        response = msdn.accounts._id.follow(_id='', reblogs=False)
+        self.assertEqual(response.status_code, 200)
+        response = msdn.accounts._id.unfollow(_id='')
+        self.assertEqual(response.status_code, 200)
+
+    def test_blocking_unblocking_account(self):
+        response = msdn.accounts._id.block(_id='')
+        self.assertEqual(response.status_code, 200)
+        response = msdn.accounts._id.unblock(_id='')
+        self.assertEqual(response.status_code, 200)
