bd052f687d1b63ac7b8fba24ff7b46391f194f8b
AltTether <200351p@ugs.kochi-tech.ac.jp>
2018-12-02 01:33:48 +0900

    [add] add test for each endpoint(accounts fetching,getting,updating)

--- tests/test.py
+++ tests/test.py
@@ -1,13 +1,28 @@
 import os
+import json
 import unittest
 
 from msdn import Msdn
 
-base_uri = 'https://hogehoge'
+base_uri = 'https://'
 access_token = os.environ['MASTODON_ACCESS_TOKEN']
 msdn = Msdn(base_uri, access_token)
 
 class TestApi(unittest.TestCase):
-    def test_accounts_id(self):
-        response = msdn.accounts(id='30573')
+    def test_fetching_accounts(self):
+        response = msdn.accounts._id(_id='30573')
         self.assertEqual(response.status_code, 200)
+
+    def test_getting_current_user(self):
+        response = msdn.accounts.verify_credentials()
+        self.assertEqual(response.status_code, 200)
+
+    def test_updating_current_user(self):
+        response = msdn.accounts.update_credentials()
+        self.assertEqual(response.status_code, 200)
+
+    def test_updating_current_user_display_name(self):
+        display_name = 'fugafuga'
+        response = msdn.accounts.update_credentials(display_name=display_name)
+        updated_display_name = json.loads(response.text)['display_name']
+        self.assertEqual(updated_display_name, display_name)
