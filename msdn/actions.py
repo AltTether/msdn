213c12eba627cb7b6f66444ba79c52e96bab9d1d
AltTether <200351p@ugs.kochi-tech.ac.jp>
2018-12-05 12:38:36 +0900

    [add] add authorizing and rejecting follow request into post action

--- msdn/actions.py
+++ msdn/actions.py
@@ -1,2 +1,2 @@
-POST_ACTIONS = ['follow', 'unfollow', 'block', 'unblock', 'mute', 'unmute', 'pin', 'unpin', 'apps']
+POST_ACTIONS = ['follow', 'unfollow', 'block', 'unblock', 'mute', 'unmute', 'pin', 'unpin', 'apps', 'authorize', 'reject']
 PATCH_ACTIONS = ['update_credentials']
