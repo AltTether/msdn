6a1cf8354679f3befe3cbefdae409e75cd07e807
AltTether <200351p@ugs.kochi-tech.ac.jp>
2018-12-09 22:29:35 +0900

    [add] adding follows into post actions

--- msdn/actions.py
+++ msdn/actions.py
@@ -1,2 +1,2 @@
-POST_ACTIONS = ['follow', 'unfollow', 'block', 'unblock', 'mute', 'unmute', 'pin', 'unpin', 'apps', 'authorize', 'reject']
+POST_ACTIONS = ['follow', 'unfollow', 'block', 'unblock', 'mute', 'unmute', 'pin', 'unpin', 'apps', 'authorize', 'reject', 'follows']
 PATCH_ACTIONS = ['update_credentials']
